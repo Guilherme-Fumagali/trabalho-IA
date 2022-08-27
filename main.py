4c8c4ccf658ab0f2eec7e99df148248fa3cd4c5f
= <=>
2022-08-27 12:18:00 -0300

    Caminho como uma váriavel

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import k_means
 import single_complete_link
 
 def main():
-    arq = open("/home/gfumagali/Documents/Trabalho IA/trabalho-IA/datasets/simpsons.txt");
+    caminho = "datasets\c2ds1-2sp.txt"
+    arq = open(caminho);
     k_means.k_means(arq, 2, 10)
-    arq = open("/home/gfumagali/Documents/Trabalho IA/trabalho-IA/datasets/simpsons.txt");
+    arq = open(caminho);
     single_complete_link.single_link(arq, 2, 5)
-    arq = open("/home/gfumagali/Documents/Trabalho IA/trabalho-IA/datasets/simpsons.txt");
+    arq = open(caminho);
     single_complete_link.complete_link(arq, 2, 5)
 
 main()
